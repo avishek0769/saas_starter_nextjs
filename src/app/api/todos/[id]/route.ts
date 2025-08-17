9dce1e54e1034171ef17b2f745f7ad8eb19b5f88
avishek0769 <avishekadhikary42@gmail.com>
2025-08-17 23:28:45 +0530

    Create todo get/post/put/delete apis

---
+++ src/app/api/todos/[id]/route.ts
@@ -0,0 +1,71 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/../lib/prisma";
+
+type ParamsType = {
+    params: {
+        id: string;
+    }
+}
+
+export async function PUT(req: NextRequest, { params }: ParamsType) {
+    const { userId } = await auth()
+    if (!userId) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const todoId = params.id;
+    if (!todoId) {
+        return NextResponse.json({ error: "Todo id is not present" }, { status: 401 });
+    }
+
+    try {
+        const { completed } = await req.json()
+
+        const updatedTodo = await prisma.todo.update({
+            where: {
+                id: todoId,
+                userId
+            },
+            data: { completed }
+        })
+        if (!updatedTodo) {
+            return NextResponse.json({ error: "Todo id is wrong" }, { status: 401 });
+        }
+        return NextResponse.json(updatedTodo, { status: 200 })
+    }
+    catch (error) {
+        return NextResponse.json(
+            { error: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
+}
+
+export async function DELETE(req: NextRequest, { params }: ParamsType) {
+    const { userId } = await auth()
+    if (!userId) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const todoId = params.id;
+    if (!todoId) {
+        return NextResponse.json({ error: "Todo id is not present" }, { status: 401 });
+    }
+
+    try {
+        await prisma.todo.delete({
+            where: {
+                id: todoId,
+                userId
+            }
+        })
+        return NextResponse.json({ message: "Todo deleted" }, { status: 200 })
+    }
+    catch (error) {
+        return NextResponse.json(
+            { error: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
+}
---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,92 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/../lib/prisma";
+
+const ITEMS_PER_PAGE = 10
+
+export async function POST(req: NextRequest) {
+    const { userId } = await auth()
+    if (!userId) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id: userId },
+            include: { todos: true }
+        })
+        if (!user) {
+            return NextResponse.json({ error: "User not found" }, { status: 402 });
+        }
+
+        if (!user.isSubscribed && user.todos.length >= 3) {
+            return NextResponse.json(
+                { error: "Free users can only create up to 3 todos. Please subscribe for more." },
+                { status: 403 }
+            );
+        }
+
+        const { title } = await req.json()
+        const newTodo = await prisma.todo.create({
+            data: {
+                title,
+                userId
+            }
+        })
+        return NextResponse.json(newTodo, { status: 200 })
+    }
+    catch (error) {
+        return NextResponse.json(
+            { error: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
+}
+
+export async function GET(req: NextRequest) {
+    const { userId } = await auth()
+    if (!userId) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const page = parseInt(req.nextUrl.searchParams.get("page") || "1")
+    const query = req.nextUrl.searchParams.get("query") || ""
+
+    try {
+        const todos = await prisma.todo.findMany({
+            where: {
+                userId,
+                title: {
+                    contains: query,
+                    mode: "insensitive"
+                },
+            },
+            skip: (page - 1) * ITEMS_PER_PAGE,
+            take: ITEMS_PER_PAGE
+        })
+    
+        const totalItems = await prisma.todo.count({
+            where: {
+                userId,
+                title: {
+                    contains: query,
+                    mode: "insensitive"
+                }
+            }
+        })
+
+        const totalPages = Math.ceil(totalItems / page)
+
+        return NextResponse.json({
+            todos,
+            totalPages,
+            currentPage: page
+        }, { status: 200 })
+    }
+    catch (error) {
+        return NextResponse.json(
+            { error: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
+}
