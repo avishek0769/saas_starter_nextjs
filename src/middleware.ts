0ddb801511df49b3287198a460f1f101cc92cc9c
avishek0769 <avishekadhikary42@gmail.com>
2025-08-23 15:47:40 +0530

    Create the home page and edit middleware to stop redirection in api routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,24 +6,27 @@ const isProtectedRoute = createRouteMatcher(["/admin/dashboard", "/dashboard"])
 export default clerkMiddleware(async (auth, req) => {
     const { userId } = await auth()
 
-    if(!userId && isProtectedRoute(req)) {
+    if (req.nextUrl.pathname.startsWith("/api")) {
+        return NextResponse.next();
+    }
+    if (!userId && isProtectedRoute(req)) {
         return NextResponse.redirect(new URL("/sign-in", req.url))
     }
-    if(userId) {
+    if (userId) {
         const user = await (await clerkClient()).users.getUser(userId)
         const role = user.publicMetadata.role as string | undefined
 
         try {
-            if(role == "admin" && req.nextUrl.pathname == "/dashboard"){
+            if (role == "admin" && req.nextUrl.pathname == "/dashboard") {
                 return NextResponse.redirect(new URL("/admin/dashboard", req.url))
             }
-            if((!role || role != "admin") && req.nextUrl.pathname == "/admin/dashboard"){
+            if ((!role || role != "admin") && req.nextUrl.pathname == "/admin/dashboard") {
                 return NextResponse.redirect(new URL("/dashboard", req.url))
             }
-            if(!isProtectedRoute(req)){
+            if (req.nextUrl.pathname.startsWith("/sign")) {
                 return NextResponse.redirect(
                     new URL(
-                        role == "admin"? "/admin/dashboard" : "/dashboard",
+                        role == "admin" ? "/admin/dashboard" : "/dashboard",
                         req.url
                     )
                 )
@@ -37,10 +40,5 @@ export default clerkMiddleware(async (auth, req) => {
 })
 
 export const config = {
-    matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-    ],
-}
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
